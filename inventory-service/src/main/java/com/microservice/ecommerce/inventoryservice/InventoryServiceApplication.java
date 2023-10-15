e27222800f477ab4ae1bc3f7fb7d92a73d8543f2
Aryan20apr <aryansingh20nov@gmail.com>
2023-10-15 22:48:54 +0530

    Configuring eureka server and discovery client

--- inventory-service/src/main/java/com/microservice/ecommerce/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/microservice/ecommerce/inventoryservice/InventoryServiceApplication.java
@@ -3,12 +3,13 @@ package com.microservice.ecommerce.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-
 import com.microservice.ecommerce.inventoryservice.modals.Inventory;
 import com.microservice.ecommerce.inventoryservice.repositories.InventoryRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
---
+++ inventory-service/src/main/java/com/microservice/ecommerce/inventoryservice/config/EurekaConfig.java
@@ -0,0 +1,14 @@
+// package com.microservice.ecommerce.inventoryservice.config;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+// import com.netflix.discovery.AbstractDiscoveryClientOptionalArgs;
+
+// @Configuration
+// public class EurekaConfig {
+//     @Bean
+//     public AbstractDiscoveryClientOptionalArgs discoveryClientOptionalArgs() {
+//         return new AbstractDiscoveryClientOptionalArgs<>();
+//     }
+// }
